5557094039092beed71f04dad97bd3b774e809f9
SPST-WINE <spediamoallestero@gmail.com>
2025-08-29 15:49:47 +0200

    Create next.js

---
+++ next.js
@@ -0,0 +1,3 @@
+pnpm create next-app . --ts --eslint --app --src-dir=false --import-alias "@/*"
+pnpm add -D tailwindcss postcss autoprefixer && npx tailwindcss init -p
+pnpm add firebase airtable react-hook-form zod swr lucide-react
