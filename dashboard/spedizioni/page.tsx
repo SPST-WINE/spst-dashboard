6d3dd7442a6136c3909b4866d924bbee198150b8
SPST-WINE <spediamoallestero@gmail.com>
2025-08-29 16:45:51 +0200

    Create page.tsx

---
+++ dashboard/spedizioni/page.tsx
@@ -0,0 +1,6 @@
+export const dynamic = "force-dynamic"; // evita pre-render
+import SpedizioniClient from "@/components/SpedizioniClient";
+
+export default function Page() {
+  return <SpedizioniClient />;
+}
