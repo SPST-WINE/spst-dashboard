64e9ac3a9ddf823c31cb751415bac2cd823f8ecb
SPST-WINE <spediamoallestero@gmail.com>
2025-08-29 16:54:52 +0200

    Update firebase-client.ts

--- lib/firebase-client.ts
+++ lib/firebase-client.ts
@@ -1,8 +1,14 @@
+// lib/firebase-client.ts
 import { initializeApp, getApps } from "firebase/app";
-export const app = getApps()[0] ?? initializeApp({
+import { getAuth } from "firebase/auth";
+
+const app = getApps()[0] ?? initializeApp({
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 });
+
+export { app };
+export const authClient = () => getAuth(app);
