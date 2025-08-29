24038fe9eca61151815aa68aab6889468acee577
SPST-WINE <spediamoallestero@gmail.com>
2025-08-29 16:28:06 +0200

    Update firebase-admin.ts

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,13 +1,12 @@
 import { cert, getApps, initializeApp } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
-
 const app = getApps()[0] ?? initializeApp({
-credential: cert({
-projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
-clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
-privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\n/g, "
-"),
-}),
+  credential: cert({
+    projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  }),
 });
+
 export const adminAuth = () => getAuth(app);
