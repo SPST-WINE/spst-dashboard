ac4a74d01c462f314133fe84b7c4b1e3d0dd9558
SPST-WINE <spediamoallestero@gmail.com>
2025-08-30 20:40:04 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 // app/layout.tsx  (ROOT LAYOUT)
 import './globals.css';
+import 'react-day-picker/dist/style.css';
 import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
