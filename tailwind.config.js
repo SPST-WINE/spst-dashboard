463ee578bca657ff00226cc4433557ca4a4c53c1
SPST-WINE <spediamoallestero@gmail.com>
2025-08-30 16:42:05 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   theme: {
     container: { center: true, padding: '1rem', screens: { '2xl': '1200px' } },
     extend: {
-      colors: { 'spst-blue': '#1c3e5e' },
+  colors: {
+    'spst-blue': '#1c3e5e',
+    'spst-orange': '#f7911e',
+  },
+}
     },
   },
   plugins: [],
