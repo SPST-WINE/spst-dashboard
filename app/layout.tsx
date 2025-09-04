f9973f81988412a8f61c0b44f4b1987c9194e04a
SPST-WINE <spediamoallestero@gmail.com>
2025-09-04 16:35:01 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,16 @@
 // app/layout.tsx
 import './clean.css';
 import { Inter } from 'next/font/google';
-import type { Metadata } from 'next';
-
 
 export const metadata = {
   title: 'SPST – Area riservata',
   description: 'Dashboard SPST',
-    icons: {
+  icons: {
     icon: [
-      { url: 'https://cdn.prod.website-files.com/6800cc3b5f399f3e2b7f2ffa/6859a72cac2c0604fbd192e3_favicon.ico', sizes: 'any' },
+      {
+        url: 'https://cdn.prod.website-files.com/6800cc3b5f399f3e2b7f2ffa/6859a72cac2c0604fbd192e3_favicon.ico',
+        sizes: 'any',
+      },
     ],
     shortcut: [
       'https://cdn.prod.website-files.com/6800cc3b5f399f3e2b7f2ffa/6859a72cac2c0604fbd192e3_favicon.ico',
